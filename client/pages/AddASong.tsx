03a848160f1f3360fe5e4169e8770f3e685fd9d4
Bryn van Vliet <bryn.music@gmail.com>
2025-05-05 16:16:41 +1200

    write routes for pages

---
+++ client/pages/AddASong.tsx
@@ -0,0 +1,14 @@
+import { Box, Flex } from "@chakra-ui/react"
+import AddSong from "../components/AddSong"
+
+
+
+export default function AddASong() {
+return (
+ <Flex align="center" justify="center"> 
+ <Box position="relative" display="inline-block">
+<AddSong />
+</Box>
+</Flex>
+)
+}
---
+++ client/routes.tsx
@@ -0,0 +1,18 @@
+import { createRoutesFromElements, Route } from 'react-router-dom'
+
+// import Layout from './components/Layout'
+import Home from './pages/Home'
+import AddASong from './pages/AddASong.tsx'
+import Catalogue from '.pages/Catalogue.tsx'
+
+
+export default createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+  <Route index element={<Home />} />
+  <Route path="/add-a-song" element={<AddASong />} />
+  <Route path="/catalogue" element={<Catalogue />} />
+
+  </Route>
+)
+
+//why aren't these importing? Did i miss an install?
