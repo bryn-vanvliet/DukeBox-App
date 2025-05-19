a281973d45e850f262a89261b4209ced37542039
Bryn van Vliet <bryn.music@gmail.com>
2025-05-19 21:58:56 +1200

    wrap App in Auth0Provider

--- client/index.tsx
+++ client/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from './components/App.tsx';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,13 +11,21 @@ const queryClient = new QueryClient();
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <ChakraProvider> 
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-        <App />
-        <ReactQueryDevtools />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </ChakraProvider>
+    
+    <Auth0Provider
+      domain="YOUR_DOMAIN"
+      clientId="YOUR_CLIENT_ID"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}><ChakraProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+            <ReactQueryDevtools />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </ChakraProvider>
+    </Auth0Provider>
+    
   );
 });
