f9c07dcdeae0523726660b73acde21f5128a94f4
Bryn van Vliet <bryn.music@gmail.com>
2025-05-19 22:15:27 +1200

    create Protected.tsx component

--- client/components/Protected.tsx
+++ client/components/Protected.tsx
@@ -1,1 +1,13 @@
-import {use}
+import { useAuth0 } from "@auth0/auth0-react";
+import { ReactNode } from "react";
+
+const Protected = ({ children}: { children: ReactNode}) => {
+  const { isAuthenticated, isLoading} = useAuth0()
+
+  if (isLoading) return <div>Loading...</div>
+  if (!isAuthenticated) return <div>You must be logged in to view this content.</div>
+
+  return <>{children}</>
+}
+
+export default Protected
