f5fa73d470199f899a7d727e8847286c9c7e8eb3
Bryn van Vliet <bryn.music@gmail.com>
2025-05-23 13:04:58 +1200

    write UserProfile & UserProfilePage

--- client/components/UserProfile.tsx
+++ client/components/UserProfile.tsx
@@ -1,30 +1,15 @@
-import useUserData from '../hooks/useUserData'
-import { Box, Skeleton, Text, Heading} from '@chakra-ui/react'
+// import useUserData from '../hooks/useUserData'
+import { Box, Text, Heading} from '@chakra-ui/react'
 
 import useUserDataAuth from '../hooks/useUserDataAuth'
 
 
-interface UserProfileProps {
-  userId: number | null | undefined
 
-}
+export default function UserProfile(){
+  const { data: userData, isPending} = useUserDataAuth()
 
-export default function UserProfile({ userId}: UserProfileProps){
-  const { data: userData} = useUserDataAuth()
-const { 
-  data: user, 
-  isPending: userIsPending, 
-  error: userError,
-} = useUserData(Number(userId))
-
-if (userIsPending) {
-  return <Skeleton height="300px" borderRadius="lg" />
-}
-
-if (userError) {
-  return <Text color="red.500">Whoops! Error fetching user data</Text>
-}
-if (!user) return <Text>User not found</Text>
+if (isPending) return <Text>Loading...</Text>
+if (!userData) return <Text>No user found</Text>
 
 return (
   <>
@@ -37,7 +22,7 @@ return (
   justifyContent="center"
   height="100px">
     <Heading mb={0} fontSize="2xl" textAlign="center">
-      {`User: ${user.name}`}</Heading></Box>
+      {`Welcome, ${userData.name}`}</Heading></Box>
       
       <Box 
       width="300px"
--- client/components/UserProfilePage.tsx
+++ client/components/UserProfilePage.tsx
@@ -1,39 +1,31 @@
 
-import {useParams} from 'react-router-dom'
-import { Box, Button, Flex} from '@chakra-ui/react'
+
+import { Box, Flex} from '@chakra-ui/react'
 import UserProfile from './UserProfile'
 
 export default function UserHomePage() {
-  const {id} = useParams<{ id: string}>()
-  const userId = Number(id)
-
 
 
   return (
     <Flex 
-    direction={['column', 'row']}
-    justify="space-between"
-    align="flex-start"
-    px={[4, 8]}
-    py={8}
-    gap={[8, 4]}
-    bg="#4fc3f7"
+    direction='column' 
+    justify="center"
+    align="center"
+    py={12}
+
+   
     minH="100vh"
     >
       <Box 
-      flexBasis={['100%', '35%']}
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      gap={6}
       bg="white"
-      p={6}
+      p={8}
       borderRadius="xl"
       boxShadow="lg"
-      position="relative"
-      top="100px"
+      width="90%"
+      maxW="500px"
+      textAlign="center"
       >
-        <UserProfile userId={userId} />
+        <UserProfile />
         
       </Box>
     </Flex>
